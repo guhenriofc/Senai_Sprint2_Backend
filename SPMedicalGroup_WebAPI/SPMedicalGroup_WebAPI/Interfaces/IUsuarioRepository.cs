d29b640d1086cd3fd588822adb1066e30045191e
Gustavo Santos <gustavo.santos211@senaisp.edu.br>
2021-05-05 15:31:08 -0300

    Interfaces e Repositories criados

---
+++ SPMedicalGroup_WebAPI/SPMedicalGroup_WebAPI/Interfaces/IUsuarioRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SPMedicalGroup_WebAPI.Interfaces
+{
+    interface IUsuarioRepository
+    {
+    }
+}
