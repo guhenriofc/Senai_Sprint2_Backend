394bcb1239f50027ba8678f518d2b48cb919bde1
Gustavo Santos <gustavo.santos211@senaisp.edu.br>
2021-05-05 16:43:51 -0300

    Repository Usuario feito

--- SPMedicalGroup_WebAPI/SPMedicalGroup_WebAPI/Repositories/UsuarioRepository.cs
+++ SPMedicalGroup_WebAPI/SPMedicalGroup_WebAPI/Repositories/UsuarioRepository.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿using SPMedicalGroup_WebAPI.Context;
+using SPMedicalGroup_WebAPI.Domains;
+using SPMedicalGroup_WebAPI.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace SPMedicalGroup_WebAPI.Repositories
 {
-    public class UsuarioRepository
+    public class UsuarioRepository : IUsuarioRepository
     {
+        SPMedicalGroupContext ctx = new SPMedicalGroupContext();
+        public void Cadastro(Usuario novoUsuario)
+        {
+            ctx.Usuarios.Add(novoUsuario);
 
+            ctx.SaveChanges();
+        }
+
+        public List<Usuario> Listar()
+        {
+            return ctx.Usuarios.ToList();
+        }
+
+        public Usuario Login(string Email, string Senha)
+        {
+            return ctx.Usuarios.FirstOrDefault(u => u.Email == Email && u.Senha == Senha);
+        }
     }
 }
